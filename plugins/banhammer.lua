99795b677aa895d9660afb807a91d96255c85012
farshadfaza <farshadfazafree@gmail.com>
2017-03-29 13:32:54 +0430

    Update banhammer.lua

--- plugins/banhammer.lua
+++ plugins/banhammer.lua
@@ -851,7 +851,8 @@ pre_process = pre_process
 }
 
 -- کد های پایین در ربات نشان داده نمیشوند
--- http://permag.ir
--- @permag_ir
--- @permag_bots
--- @permag
+-- ادیت توسط :
+-- @ninjatem
+-- @farshadfarokh
+-- @akambagroxxx
+-- @ninjahacker
