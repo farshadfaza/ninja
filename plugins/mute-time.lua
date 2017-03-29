803fc4b5a7ce30d117cb56194fa5f399ba6598ec
farshadfaza <farshadfazafree@gmail.com>
2017-03-29 14:00:12 +0430

    Update mute-time.lua

--- plugins/mute-time.lua
+++ plugins/mute-time.lua
@@ -35,6 +35,8 @@ return {
   run = run,
   pre_process = pre_process
 }
--- http://permag.ir
--- @permag_ir
--- @permag_bots
+-- ادیت توسط :
+-- @ninjatem
+-- @farshadfarokh
+-- @akambagroxxx
+-- @ninjahacker
